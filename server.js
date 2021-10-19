4aef408f42581b7524ecdc5b961676d5449076d7
Lucas Silva <lucassil153@gmail.com>
2021-10-19 02:45:55 -0300

    Simple Chat

---
+++ server.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var bodyParser = require('body-parser')
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var mongoose = require('mongoose');
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}))
+var Message = mongoose.model('Message',{
+  name : String,
+  message : String
+})
+var dbUrl = 'mongodb+srv://lucas:p3j9ge@simple-chat.fzmfe.mongodb.net/simple-chat?retryWrites=true&w=majority'
+
+app.get('/messages', (req, res) => {
+  Message.find({},(err, messages)=> {
+    res.send(messages);
+  })
+})
+app.get('/messages', (req, res) => {
+  Message.find({},(err, messages)=> {
+    res.send(messages);
+  })
+})
+app.post('/messages', (req, res) => {
+  var message = new Message(req.body);
+  message.save((err) =>{
+    if(err)
+      sendStatus(500);
+    io.emit('message', req.body);
+    res.sendStatus(200);
+  })
+})
+io.on('connection', () =>{
+  console.log(' Um Usuário se conectou !!')
+})
+mongoose.connect(dbUrl , (err) => { 
+    console.log('mongodb connected',err);
+ })
+var server = http.listen(3000, () => {
+  console.log('server running', server.address().port);
+});
