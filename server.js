312f2addf818e381109d0bce02b32df971687bab
Lucas Silva <lucassil153@gmail.com>
2021-10-23 14:47:17 -0300

    Personalizando formulario

--- server.js
+++ server.js
@@ -13,24 +13,21 @@ var Message = mongoose.model('Message',{
 })
 var dbUrl = 'mongodb+srv://lucas:p3j9ge@simple-chat.fzmfe.mongodb.net/simple-chat?retryWrites=true&w=majority'
 
+
 app.get('/messages', (req, res) => {
   Message.find({},(err, messages)=> {
     res.send(messages);
   })
 })
-app.get('/messages', (req, res) => {
-  Message.find({},(err, messages)=> {
-    res.send(messages);
-  })
-})
+
 app.post('/messages', (req, res) => {
-  var message = new Message(req.body);
-  message.save((err) =>{
-    if(err)
-      sendStatus(500);
-    io.emit('message', req.body);
-    res.sendStatus(200);
-  })
+     var message = new Message(req.body);
+        message.save((err) =>{
+             if(err)
+                sendStatus(500);
+                io.emit('message', req.body);
+                res.sendStatus(200);
+              })
 })
 
 
